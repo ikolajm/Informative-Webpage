45ac401483a95cc425c24f7e53409ddbee04b9e9
ikola <ikolajm@gmail.com>
2019-01-14 13:24:51 -0500

    Added a smooth scroll and scrollTop checks

--- main.js
+++ main.js
@@ -24,15 +24,39 @@
     }
 
 // Scrolltop opacity
-    let body = document.querySelector('body');
-    body.addEventListener('scroll', function() {
-        // let header = document.querySelector('header');
-        console.log(body.scrollTop);
+    // On scroll, check where scrolltop is and adjust the opacity of the header accordingly
+    $(window).scroll(function() {
+        // Check scroll top position
+        if ($(window).scrollTop() > 0) {
+            document.querySelector('header').style.opacity = '.85';
+        } else {
+            document.querySelector('header').style.opacity = '1';
+        }
     });
 
 // Hover Opacity
+    // On header hover, change the opacity to 1
+    $('header').mouseover(function() {
+        this.style.opacity = '1';
+    });
+
+    // When cursor leaves the header, check the scrollTop and determine if opacity needs to be readjusted or not
+    $('header').mouseout(function() {
+        if ($(window).scrollTop() > 0) {
+            document.querySelector('header').style.opacity = '.85';
+        } 
+    });
 
 // Smooth Scroll
+    // Check all local links
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        // Prevent default link behavior
+        event.preventDefault();
 
-// Active States
-        
+        // Animate the scroll to the links target
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
+// Active States POTENTIALLY
